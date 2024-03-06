04e77d334f5e3254075fb416d8726c33e842c6af
MikeFernando <mikera2020@outlook.com>
2024-03-06 18:08:19 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { Groups } from "@screens/Groups";
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <Groups />
+    </>
+  );
+}
